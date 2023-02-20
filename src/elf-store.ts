d6a73f16fe7d2b66ef4ab33bd4f8276b46d5e40b
Andrei Iakovlev <extern.iakovlev_andrei@allianz.com>
2023-02-20 11:23:40 +0400

    effects

--- src/elf-store.ts
+++ src/elf-store.ts
@@ -1,9 +1,34 @@
-import { createStore } from '@ngneat/elf';
-import { withEntities } from '@ngneat/elf-entities';
-import { createEffect, ofType, createAction, props } from '@ngneat/effects';
-import { trackRequestResult } from '@ngneat/elf-requests';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { createStore } from "@ngneat/elf";
+import {
+  selectActiveEntity,
+  selectAllEntities,
+  selectEntity,
+  setEntities,
+  updateEntities,
+  withEntities,
+} from "@ngneat/elf-entities";
+import {
+  createEffect,
+  ofType,
+  createAction,
+  props,
+  payload,
+  createEffectFn,
+  dispatch,
+  actionsFactory,
+  Actions,
+} from "@ngneat/effects";
+import {
+  createRequestDataSource,
+  joinRequestResult,
+  trackRequestResult,
+  withRequestsStatus,
+} from "@ngneat/elf-requests";
+import { Injectable, OnDestroy } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map, mergeMap, Observable, ReplaySubject, switchMap, tap } from "rxjs";
+import { fromFetch } from "rxjs/fetch";
+import { EffectFn } from "@ngneat/effects-ng";
 
 interface ToDo {
   userId: number;
@@ -12,30 +37,80 @@ interface ToDo {
   completed: boolean;
 }
 
-const todoStore = createStore({ name: 'todos' }, withEntities<ToDo>());
+const todoStore$ = createStore(
+  { name: "todos" },
+  withEntities<ToDo>(),
+  withRequestsStatus()
+);
 
-todoStore.update();
+const todoActions = actionsFactory("todo");
+const LoadTodos = todoActions.create("load todos");
+const UpdateTodo = todoActions.create("update todo", props<{ todo: ToDo }>());
+const UpdateTodoSuccess = todoActions.create(
+  "update todo success",
+  props<{ todo: ToDo }>()
+);
+const UpdateTodoFail = todoActions.create("update todo fail");
+export { LoadTodos, UpdateTodo, UpdateTodoSuccess, UpdateTodoFail };
 
-const loadTodos = createAction('[Todos] Load');
-const UpdateTodo = createAction('[Todos] Load', props());
-const UpdateTodoSuccess = createAction('[Todos] Load');
-const UpdateTodoFail = createAction('[Todos] Load');
+const { setSuccess, trackRequestStatus, data$ } = createRequestDataSource({
+  data$: () => todoStore$.pipe(selectAllEntities()),
+  requestKey: "todos",
+  dataKey: "todos",
+  store: todoStore$,
+});
 
-// export const loadTodos$ = createEffect((actions) =>
-//   actions.pipe(
-//     ofType(loadTodos),
-//     switchMap((todo) => todosApi.loadTodos()),
-//     tap(setTodos)
-//   )
-// );
+const todos$ = data$();
+todos$.subscribe(({ error, loading, todos }) => console.log());
 
 @Injectable()
-export class TodosRepository {
+export class TodosService {
   constructor(private http: HttpClient) {}
 
   getTodos() {
-    return this.http
-      .get('https://jsonplaceholder.typicode.com/users/1/todos')
-      .pipe(trackRequestResult(['todos']));
+    return this.http.get<ToDo[]>("https://jsonplaceholder.typicode.com/todos");
   }
+
+  updateTodo(todo: ToDo) {
+    return this.http.put<ToDo>(
+      `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
+      todo
+    );
+  }
+}
+
+@Injectable()
+export class TodosEffects extends EffectFn {
+  constructor(private todosService: TodosService) {
+    super();
+  }
+
+  private setTodos = (todos: ToDo[]) => {
+    todoStore$.update(setEntities(todos), setSuccess());
+  };
+
+  getTodosEffect$ = createEffect((actions) =>
+    actions.pipe(
+      ofType(LoadTodos),
+      switchMap(() => this.todosService.getTodos()),
+      tap(this.setTodos)
+    )
+  );
+
+  getTodos = this.createEffectFn(($: Observable<void>) => {
+    return $.pipe(
+      trackRequestStatus(),
+      switchMap(() => this.todosService.getTodos()),
+      tap(this.setTodos)
+    );
+  });
+
+  updateTodo = this.createEffectFn((todo$: Observable<ToDo>) =>
+    todo$.pipe(
+      switchMap((todo) => this.todosService.updateTodo(todo)),
+      tap({
+        next: (todo: ToDo) => todoStore$.update(updateEntities(todo.id, todo)),
+      })
+    )
+  );
 }
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,48 @@
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { TodosRepository } from './elf-store';
-import { HttpClientModule } from '@angular/common/http';
+import "zone.js/dist/zone";
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { LoadTodos, TodosEffects, TodosService } from "./elf-store";
+import { HttpClientModule } from "@angular/common/http";
+import { Actions, provideEffects, provideEffectsManager } from "@ngneat/effects-ng";
 
 @Component({
-  selector: 'my-app',
+  selector: "my-app",
   standalone: true,
-  providers: [TodosRepository]
-  imports: [CommonModule, HttpClientModule],
+  providers: [TodosService],
+  imports: [CommonModule],
   template: `
-    <h1>Hello from {{name}}!</h1>
+    <h1>Hello from {{ name }}!</h1>
     <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
+      Learn more about Angular
     </a>
   `,
 })
-export class App {
-  name = 'Angular';
+export class App implements OnInit {
+  name = "Angular";
+
+  constructor(
+    private todosRepo: TodosService,
+    private todosEffects: TodosEffects,
+    private actions: Actions
+
+  ) {}
+
+  ngOnInit(): void {
+      // direct call way
+      this.todosRepo.getTodos().subscribe();
+
+      // direct call effect
+      this.todosEffects.getTodos();
+      // actions like way
+      this.actions.dispatch(LoadTodos());
+  }
 }
 
-bootstrapApplication(App);
+bootstrapApplication(App, {
+  providers: [
+    HttpClientModule,
+    provideEffectsManager(),
+    provideEffects(TodosEffects),
+  ],
+});
